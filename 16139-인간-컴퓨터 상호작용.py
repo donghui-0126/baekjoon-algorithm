65522e39f9e81f397ac62e8824ca8cc828cfa01d
donghui <lukehui@naver.com>
2022-08-16 16:30:23 +0900

    Update 16139-인간-컴퓨터 상호작용.py
    풀어서 틀렸는데 논리는 맞았음..!
    
    오류를 찾고싶으나 너무 힘들어서 돔황챠

--- 16139-인간-컴퓨터 상호작용.py
+++ 16139-인간-컴퓨터 상호작용.py
@@ -1,30 +1,35 @@
-from posixpath import split
 import sys
-from unittest.mock import sentinel
 input = sys.stdin.readline
 
 sentence = input().strip()
 q = int(input())
-question_list = []
-
-for _ in range(q):
-    question_list.append(input().strip())
-
-sumation = 0
-S = [] 
-for L in (sentence):
-    if L == question_list[0][0]:
-        sumation += 1
-    S.append(sumation)
+question_list = [list(input().split()) for _____ in range(q)]
 
 
-for i in range(q):
-    currnet_question = question_list[i]
+S = [0 for ___ in range(26)]
+Letter = []
+
+for i in range(q):     
+    li = []
+    sumation = 0
+    if question_list[i][0] not in Letter:
+        for L in sentence:
+            if L == question_list[i][0]:
+                sumation += 1         
+            li.append(sumation)
+        S[ord(question_list[i][0])-97] = li
+        Letter.append(question_list[i][0])
+
+for j in range(q):
+    currnet_question = question_list[j]
     letter = currnet_question[0]
-    start, end =  map(int,currnet_question[1:].split())
+    start, end =  int(currnet_question[1]), int(currnet_question[2])
     if start != 0:
-        print(S[end] - S[start-1])
+        print(S[ord(question_list[i][0])-97][end] - S[ord(question_list[i][0])-97][start-1])
     else:
-        print(S[end])
+        print(S[ord(question_list[i][0])-97][end])
 
-#이거 왜 런타임 에러뜸???
+#여기까지는 50점 
+# list.index 메소드 때문에 아무래도 50점 밖에 못받은 것 같다.
+# 악 오류뜸... 근데 이렇게 하는 건 맞댱...
+# 너무 하기가 싫어서 여기까지..!
